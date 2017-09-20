ebe69f3c5558b700c1649bb0efddf6f5ae42a23c
dpa35 <dpa35@asb9838nu-b15.csil.sfu.ca>
2017-09-20 16:12:51 -0700

    t4 submission

---
+++ 1/t5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void)
+{
+	int result;
+	while (result != EOF)
+	{
+	float i;
+	result = scanf("%f", &i);
+	printf("%.0lf %.0lf %.0f\n", floor(i), round(i), ceil(i));
+	}
+	printf("Done.\n");
+	return 0;
+}
