5470c295eddd6ecc0665dae34078d20bf24d0781
David Pham <dpa35@sfu.ca>
2017-09-24 13:47:15 -0700

    t5.c submission

--- 1/t5.c
+++ 1/t5.c
@@ -7,9 +7,16 @@ int main (void)
 	//while loop until EOF indicator detected//
 	while (result != EOF)
 	{
-	float i;
-	result = scanf("%f", &i);
-	printf("%.0lf %.0lf %.0f\n", floor(i), round(i), ceil(i));
+		float i;
+		result = scanf("%f", &i);
+		if(sizeof(i)==4)
+		{
+		printf("%.0lf %.0lf %.0f\n", floor(i), round(i), ceil(i));
+		}
+		else
+		{
+		printf("scan error not a float value\n");
+		}
 	}
 	printf("Done.\n");
 	return 0;
