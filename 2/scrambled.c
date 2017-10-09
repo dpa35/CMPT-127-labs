71b70b9eaaa64ee56509ead46e6f21119278b9ca
David Pham <dpa35@sfu.ca>
2017-10-09 15:41:36 -0700

    scrambled is working

---
+++ 2/scrambled.c
@@ -0,0 +1,32 @@
+int scrambled(unsigned int a[], unsigned int b[], unsigned int len){
+	int x = 0;
+	int y = 0;
+	int count1 = 0;
+	int count2 = 0;
+	int testvalue =0;
+
+	for(x=0; x<len; x++){
+		testvalue = a[x];
+		for(y=0; y<len; y++){
+			if(testvalue==b[y]){
+				count1++;
+			}
+		}
+	}
+	for(x=0; x<len; x++){
+		testvalue= b[x];
+		for(y=0; y<len; y++){
+			if(testvalue == a[y]){
+				count2++;
+			}
+		}
+	}
+	if (count1==count2){
+		return 1;
+	}
+	return 0;
+	
+			
+			
+		
+}
