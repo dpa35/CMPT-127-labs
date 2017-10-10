e4941dd14c9aacf0512987f735af0285e543f24a
David Pham <dpa35@sfu.ca>
2017-10-10 16:34:32 -0700

    please work

--- 2/verticalgraph.c
+++ 2/verticalgraph.c
@@ -8,7 +8,9 @@ int main(void){
 	int x = 0;
 	int largestValue = 0; 
 	int currentValue = 0;
-	while((i== 0) || (i<80 && currentValue != EOF)){
+	currentValue = scanf("%d", &input[i]);
+	i++;
+	while((i<80 && currentValue != EOF)){
 		currentValue = scanf("%d", &input[i]);
 		i++;
 	}
