9faf3a70e7b9779c7fcc37c49c21396d37c8b5e0
David Pham <dpa35@sfu.ca>
2017-10-10 14:54:16 -0700

    5th time is the try

--- 2/count.c
+++ 2/count.c
@@ -5,19 +5,26 @@ int main (void){
 	unsigned long wordCount = 0;
 	unsigned long charCount = 0;
 	unsigned long lineCount = 0;
+	int oldChar = 0;
 	int curChar = getchar();
 
 	while (curChar != EOF){
 		charCount++;
-		if(!(curChar>64 && curChar<91 || curChar> 96 && curChar < 123 || curChar == 39)){
+		//if current char is not a-z/A-Z/' and char before was then a word was counted
+		if(!(curChar>64 && curChar<91 || curChar> 96 && curChar < 123 || curChar == 39)&&
+		  (oldChar>64 && oldChar<91 || oldChar>96 && oldChar <123 || oldChar==39)){	
 			wordCount++;
-		}
+		  }
+
+		
+		//\n is detected
 		if(curChar==10){
 			lineCount++;
 		}
+		oldChar = curChar;
 		curChar = getchar();
 	}
-	charCount --;
+	
 	printf("%lu %lu %lu\n", charCount, wordCount, lineCount);
 	return 0;
 }
