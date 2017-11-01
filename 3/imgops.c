6012e553365e320cfe8ba76006dacbbab811e9bf
dpa35 <dpa35@asb9838nu-c06.csil.sfu.ca>
2017-11-01 14:48:26 -0700

    another try

--- 3/imgops.c
+++ 3/imgops.c
@@ -301,7 +301,7 @@ void normalize( uint8_t array[],
 	//where a and b are desired min and max respectively
 	//off by one may be truncation error prior to rounding 
 		test = ((255)*(array[x]-darkest))/(lightest-darkest);
-		array[x] = round(test);
+		array[x] = test;
 	
 	}
     // your code here
