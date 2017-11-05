f489e389b60af77516c72f11fa49bc7941949193
David Pham <dpa35@sfu.ca>
2017-11-05 14:38:09 -0800

    task 1

--- 5/intarr.c
+++ 5/intarr.c
@@ -7,24 +7,12 @@
 #include "intarr.h"
 
 
-/* Structure type that encapsulates our safe int array. */
-typedef struct {
-  int* data;
-  unsigned int len;
-} intarr_t;
 
-/* A type for returning status codes */
-typedef enum {
-  INTARR_OK,
-  INTARR_BADARRAY,
-  INTARR_BADINDEX,
-  INTARR_BADALLOC,
-  INTARR_NOTFOUND
-} intarr_result_t;
 
 // Create a new intarr_t with initial size len.  If successful
 // (i.e. memory allocation succeeds), returns a pointer to a
 // newly-allocated intarr_t.  If unsuccessful, returns a null pointer.
+
 intarr_t* intarr_create( unsigned int len ){
 	//code here
 	//allocate space for intarr_t
