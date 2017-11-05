c8add9a5d0321753208082ca9c78fef90394a40b
David Pham <dpa35@sfu.ca>
2017-11-05 14:23:30 -0800

    task 1

--- 5/intarr.c
+++ 5/intarr.c
@@ -5,22 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Structure type that encapsulates our safe int array. */
-typedef struct {
-  int* data;
-  unsigned int len;
-} intarr_t;
 
-/* A type for returning status codes */
-typedef enum {
-  INTARR_OK,
-  INTARR_BADARRAY,
-  INTARR_BADINDEX,
-  INTARR_BADALLOC,
-  INTARR_NOTFOUND
-} intarr_result_t;
-
-/* LAB 5 TASK 1 */
 
 // Create a new intarr_t with initial size len.  If successful
 // (i.e. memory allocation succeeds), returns a pointer to a
@@ -31,7 +16,7 @@ intarr_t* intarr_create( unsigned int len ){
 	intarr_t* intarrA = malloc(sizeof(intarr_t));
 	//allocate space for data in intarr_t
 	intarrA -> data= malloc((len)*sizeof(unsigned int));
-	if(intarrA==0){
+	if(intarrA==NULL){
 		return NULL;
 	}
 	return intarrA;
@@ -41,7 +26,7 @@ intarr_t* intarr_create( unsigned int len ){
 // frees all memory allocated for ia. If the pointer is null, do
 // nothing. If the ia->data is null, do not attempt to free it.
 void intarr_destroy( intarr_t* ia ){
-	if(ia==0){
+	if(ia==NULL || ia-> data==NULL){
 		return;
 	}
 	free(ia);
