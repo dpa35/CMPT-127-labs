f9ae11ea2b0a18630e110748a32ec57eca50faaa
David Pham <dpa35@sfu.ca>
2017-11-07 22:18:26 -0800

    task 7

--- 5/intarr.c
+++ 5/intarr.c
@@ -244,6 +244,37 @@ intarr_result_t intarr_pop( intarr_t* ia, int* i ){
 // INTARR_OK, otherwise return INTARR_BADALLOC. If ia is null, return
 // INTARR_BADARRAY.
 intarr_result_t intarr_resize( intarr_t* ia, unsigned int newlen ){
+	if(ia==0){
+		return INTARR_BADARRAY;
+	}
+	//use pop and push functions w/ difference between newlen and len
+
+	int x=0;
+	int * i= malloc(sizeof(int));
+	//printf("%i\n", (newlen-(ia->len)));
+	//if newlen is less than original array
+	if(newlen < (ia->len)){
+		for(x=0; x<((ia->len)-newlen)+1;x++){
+			printf("I did a thing\n");
+			if((intarr_pop(ia,i))== INTARR_BADALLOC){
+				return INTARR_BADALLOC;
+			}
+			
+			
+		}
+	}
+	
+	if(newlen>(ia->len)){
+		for(x=0;x<(newlen-(ia->len))+1;x++){
+			printf("I did a thing\n");
+			if((intarr_push(ia,0))== INTARR_BADALLOC){
+				return INTARR_BADALLOC;
+			}
+		}
+
+	}
+	return INTARR_OK;
+
 
 }
 
