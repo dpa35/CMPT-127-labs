92584920de8941cb6e289eb564113689d68a051a
dpa35 <dpa35@sfu.ca>
2017-12-01 13:47:44 -0800

    t1

--- 8/t1.c
+++ 8/t1.c
@@ -89,7 +89,7 @@ int point_array_remove( point_array_t* pa, unsigned int i ){
 	//valid index returned 1? empty array?
 	pa->points=realloc(pa->points, (((pa->len)-1) * sizeof(point_t)));
 		printf("this is working\n");
-	if(pa->points == NULL || pa->len ==0){
+	if(pa->points == NULL){
 		return 1;
 	}
 	pa->len --;
