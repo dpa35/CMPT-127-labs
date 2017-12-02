b5c5b01ab45084354a20cf84636a4337b4c8ea8e
dpa35 <dpa35@sfu.ca>
2017-12-02 12:06:00 -0800

    t2.c

--- 8/t2.c
+++ 8/t2.c
@@ -35,14 +35,18 @@ int point_array_append( point_array_t* pa, point_t* p ){
 	if(p==NULL){
 		return 1;
 	}
-	
+	pa->reserved--;
 	//initial append (empty array)
 	//malloc 2 slots (1 for intial point_t and 1 for reserved point_t increase reserve)
 	if(pa->reserved ==0 && pa->len ==0){
 		pa->points = malloc(2 * sizeof(point_t));
+		if(pa->points==NULL){
+				return 1;
+		}
 		pa->reserved= 1;
+		printf("I'm working\n");
 	}
-	pa->reserved --;
+	
 	//decrease reserved space by 1
 	//check to see if more allocation is required, reallocate if necessary
 	//array should double in length with each allocation
@@ -50,6 +54,7 @@ int point_array_append( point_array_t* pa, point_t* p ){
 		pa->points=realloc(pa->points, ((pa->len)*2)*sizeof(point_t));
 		if(pa->points ==NULL){
 			return 1;
+			printf("im working\n");
 		}
 		//reserved space doubles from previous value by making it length
 		pa->reserved= pa->len;
