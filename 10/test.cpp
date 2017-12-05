1429e86430b14f3782b1a9a89c4f87942ca3e18a
dpa35 <dpa35@sfu.ca>
2017-12-05 13:53:41 -0800

    task3

--- 10/test.cpp
+++ 10/test.cpp
@@ -1,11 +1,19 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "imageh.hpp"
+#include "image3.hpp"
 
 int main(){
   Image* img = new Image();
   
+    img->resize(20, 20, 30);
+    printf("size of rows: %i \n", img->rows);
+    printf("size of cols: %i \n", img->cols);
+
+
+    
+
+
   delete img;
 
 
